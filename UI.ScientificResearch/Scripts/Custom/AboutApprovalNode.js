0a52be2d5a72ce539f07625510a3a87b0365d076
DESKTOP-BV4QAFB\jim <jim.miracle.wang@hotmail.com>
2017-07-31 21:29:27 +0800

    1. 文件上传功能 添加对文件类型的过滤 2. 文件上传列表和 报名列表中添加一个保存当前项目id的隐藏input

--- UI.ScientificResearch/Scripts/Custom/AboutApprovalNode.js
+++ UI.ScientificResearch/Scripts/Custom/AboutApprovalNode.js
@@ -2,10 +2,54 @@
 //检查上传文件的大小
 function CheckFileSize() {
     var file = document.getElementById("uploadFile");
-    var fileSize = file.files.item(0).size / (1024 * 1024);
-    if (fileSize > 100) {
-        alert("上传的文件限制在100M，请重新选择要上传的文件！");
+    var fileSize = 0;
+    var filepath = file.value;
+    var filetypes = [".rar", ".txt", ".zip", ".doc", ".ppt", ".xls", ".pdf", ".docx", ".xlsx"];
+
+    if (filepath) {
+        var isnext = false;
+        var fileend = filepath.substring(filepath.lastIndexOf("."));
+        if (filetypes && filetypes.length > 0) {
+            for (var i = 0; i < filetypes.length; i++) {
+                if (filetypes[i] == fileend) {
+                    isnext = true;
+                    break;
+                }
+            }
+        }
+        if (!isnext) {
+            alert("不接受此文件类型！");
+            document.getElementById("uploadFile").value = "";
+            return false;
+        }
+    } else {
+        return false;
+    }
+
+    var isIE = /msie/i.test(navigator.userAgent) && !window.opera;
+    if (isIE && !file.files) {
+        var filePath = file.value;
+        var fileSystem = new ActiveXObject("Scripting.FileSystemObject");
+        if (!fileSystem.FileExists(filePath)) {
+            alert("附件不存在，请重新输入！");
+            return false;
+        }
+        var file = fileSystem.GetFile(filePath);
+        fileSize = file.Size;
+    } else {
+        fileSize = file.files[0].size;
+    }
+
+    var size = fileSize / 1024;
+    if (size > filemaxsize) {
+        alert("附件大小不能大于" + filemaxsize / 1024 + "M！");
         document.getElementById("uploadFile").value = "";
+        return false;
+    }
+    if (size <= 0) {
+        alert("附件大小不能为0M！");
+        document.getElementById("uploadFile").value = "";
+        return false;
     }
 }
 //填写申请书过程中已有文件
